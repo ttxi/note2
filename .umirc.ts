c3a2854bc8e114b5ff107b1c4fc2730ff1b13845
xitiantian <tiantian.xi@aminer.cn>
2022-07-20 18:05:09 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -5,4 +5,6 @@ export default defineConfig({
   mode: 'site',
   // more config: https://d.umijs.org/config
   locales: [['zh-CN', '中文']],
+  // base: '/note2/',
+  publicPath: '/note2/',
 });
--- push.sh
+++ push.sh
@@ -3,18 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-yarn build
-
-# 进入生成的文件夹
-cd dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'init'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
@@ -22,8 +13,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f git@github.com:ttxi/note2.git master:gh-pages
+# git push git@github.com:ttxi/note.git master
+git push
 
-echo '111111111'
-
-cd -
+# cd -
